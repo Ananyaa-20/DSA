54899fc166ca8be96eb9341a2f349635fc1a8df7
Ananya Gupta <72341852+Ananyaa-20@users.noreply.github.com>
2022-11-12 20:57:39 +0530

    Find Median from Data Stream

---
+++ MedianFinder.java
@@ -0,0 +1,28 @@
+//Find Median from Data Stream
+class MedianFinder {
+    PriorityQueue<Integer>minHeap = null;
+    PriorityQueue<Integer>maxHeap = null;
+
+    public MedianFinder() {
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+    }
+    
+    public void addNum(int num) {
+        minHeap.offer(num);
+        maxHeap.offer(minHeap.poll());
+        
+        if(minHeap.size()<maxHeap.size()){
+            minHeap.offer(maxHeap.poll());
+        }
+        
+    }
+    
+    public double findMedian() {
+        if(minHeap.size()>maxHeap.size()){
+            return minHeap.peek();
+        } else {
+            return (minHeap.peek()+maxHeap.peek())/2.0;
+        }
+    }
+}
