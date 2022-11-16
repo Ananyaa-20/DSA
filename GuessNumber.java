27b758bafb59337d6959aa9d7ca82b8a91ff2d5a
Ananya Gupta <72341852+Ananyaa-20@users.noreply.github.com>
2022-11-16 21:16:44 +0530

    Create GuessNumber.java

---
+++ GuessNumber.java
@@ -0,0 +1,19 @@
+public class GuessNumber extends GuessGame {
+    public int guessNumber(int n) {
+        int low = 1;
+        int high = n;
+        while(low<=high){
+            int mid = low + ((high-low)/2);
+            int result = guess(mid);
+            if(result==0){
+                return mid;
+            } else if(result == 1){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+}
