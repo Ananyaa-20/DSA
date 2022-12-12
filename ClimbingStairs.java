6233336f2bc8e29954e5b17289b0f20ddf4b7072
Ananya Gupta <72341852+Ananyaa-20@users.noreply.github.com>
2022-12-12 21:08:15 +0530

    Update ClimbingStairs.java

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,15 +1,14 @@
-class Solution {
+clasclass Solution {
     public int climbStairs(int n) {
-            int arr[] = new int[n+1];
-    arr[0] = 0;
-    arr[1] = 1;
-    arr[2] = 2;
-    
-    for(int i=3;i<arr.length;i++){
-        arr[i] = arr[i-1] + arr[i-2]; 
-    }
-    
-    return arr[n];
+    int ways=1;
+        for(int i=1;i<=n/2;i++){
+        double sum=1;
+            for(int j=i;j<2*i;j++){
+                sum *= (double)(n-j)/(j-i+1);
+             }
+        ways+=sum;
+        }
+        return ways;
 }
 
 }
